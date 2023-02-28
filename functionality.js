2cef20e9bf624cc5a81e6c2b8af1f1c67fdf3ad8
arjun2425 <126446372+arjun2425@users.noreply.github.com>
2023-02-28 18:30:37 +0530

    Add files

---
+++ functionality.js
@@ -0,0 +1,30 @@
+var form = document.getElementById('contactForm');
+
+// Listen for form submission
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  // Get form data
+  var name = form.elements.name.value;
+  var email = form.elements.email.value;
+  var message = form.elements.message.value;
+
+  // Get a reference to the database service
+  var database = firebase.database();
+
+  // Write the data to the database
+  database.ref('contacts').push({
+    name: name,
+    email: email,
+    message: message
+  });
+
+  // Clear the form
+  form.reset();
+
+
+  // Show a success message
+  alert('Thank you for your message!, We appreciate your feedback and will get back to you soon.');
+});
+
+
